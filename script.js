dc81d280c58591f4f6f752aec9f352d20065f5ef
DarknessKALU <darknesskalu@gmail.com>
2026-05-29 11:24:06 +0530

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ─── Counter ───
 const subCountEl = document.getElementById("subCount");
-let target = 850; // ← change to your real goal
+let target = 1000; // ← change to your real goal
 let count = 0; 
 
 const counter = setInterval(() => {
